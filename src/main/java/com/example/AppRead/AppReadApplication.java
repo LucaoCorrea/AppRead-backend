76192b3073915f8ba0a3fdd04c872ad595424645
LucaoCorrea <lucascorrea1505@gmail.com>
2024-02-20 00:56:09 -0300

    Add Inicial Files

---
+++ src/main/java/com/example/AppRead/AppReadApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppRead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AppRead/AppReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppRead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
