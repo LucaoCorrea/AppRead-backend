630f45e5145fd80c3a5340ac02d8bc077c58ed30
LucaoCorrea <lucascorrea1505@gmail.com>
2024-02-21 01:36:33 -0300

    creating secret keys

---
+++ src/main/java/com/example/AppRead/config/JwtService.java
@@ -0,0 +1,34 @@
+package com.example.AppRead.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.websocket.Decoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+
+public class JwtService {
+
+    private static final String SECRET_KEY = "33DFD46124DC477AB6C719C77BC7F";
+    public String extractUsername(String token) {
+        return null;
+    }
+    private Claims exctractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
